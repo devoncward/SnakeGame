384f9669d5e70209a76fab22beb68aaae120b626
Devon Ward <devoncward@gmail.com>
2020-06-26 14:20:18 -0600

    Added notes for derek and me for tomorrow

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "Display.h"
 
+//Will also need to create state machine here as example
+
+//Note for Derek: use printf instead of cout, I like it better, and it offers more control in printing to the console
+
 int main() {
     int foo = Display_drawSnake();
     printf("If this prints a 1, then my display.h function worked: %d\n", foo);
